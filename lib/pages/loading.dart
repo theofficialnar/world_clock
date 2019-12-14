1921a329b52ca1b7180b03361dc038e6ccccb2ac
Nar Cuenca <narc.ph@gmail.com>
2019-12-14 21:22:10 +0800

    feat: Uploading files

---
+++ lib/pages/loading.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setupWorldTime() async {
+    WorldTime instance =     WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png');
+    await instance.getTime();
+
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'time': instance.time,
+      'flag': instance.flag,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitChasingDots(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      ),
+    );
+  }
+}
